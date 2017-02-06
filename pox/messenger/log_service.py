b37fc4ab25c28e4ea8e0ca1d3982c3c0a94fa8b4
william <williamvitorino3@gmail.com>
2017-02-06 11:06:46 -0300

    Documantação do log_service .py adicionada.

--- pox/messenger/log_service.py
+++ pox/messenger/log_service.py
@@ -25,12 +25,25 @@ log = core.getLogger()
 
 # These attributes are copied verbatim from the log record
 _attributes = [
-  'created','filename','funcName','levelname','levelno','lineno',
-  'module','msecs','name','pathname','process','processName',
-  'relativeCreated','thread','threadName','args',
+  'created', 'filename', 'funcName', 'levelname', 'levelno', 'lineno',
+  'module', 'msecs', 'name', 'pathname', 'process', 'processName',
+  'relativeCreated', 'thread', 'threadName', 'args',
 ]
 
 class LogMessenger (logging.Handler):
+  """
+  Um Python logging.Handler que é um serviço de mensageiro
+
+  Aceita dicionários com informações de configuração:
+  VALOR CHAVE
+  Level Nível de log mínimo para output (provavelmente um de CRITICAL, ERROR,
+              AVISO, INFO ou DEBUG)
+  Formato fmt argumento para logging.Formatter
+  DateFormat datefmt argumento para logging.Formatter
+  Json true se você quiser um monte de atributos do LogRecord para ser
+               incluído. Em alguns casos, essas são versões
+               Originais são objetos e não os picamos / jsonpickle.
+  """
   """
   A Python logging.Handler that is a messenger service
 
@@ -62,6 +75,11 @@ class LogMessenger (logging.Handler):
     self._listeners = autoBindEvents(self, connection) #GC?  Weak?
 
   def _processParameters (self, params):
+    """
+    Processa os parêmetros de envio de mensagem.
+    :param params: Parametros à serem processados.
+    :return: Sem retorno.
+    """
     if "level" in params:
       self.setLevel(params["level"])
     if "json" in params:
@@ -83,6 +101,12 @@ class LogMessenger (logging.Handler):
       self.setFormatter(logging.Formatter(self._format, self._dateFormat))
 
   def _handle_MessageReceived (self, event, msg):
+    """
+    Processa os parâmetros do evento.
+    :param event: Evento que chama este método.
+    :param msg: Mensagem de parâmetro.
+    :return: Sem retorno.
+    """
     if event.con.isReadable():
       r = event.con.read()
       if type(r) is dict:
@@ -91,13 +115,24 @@ class LogMessenger (logging.Handler):
           event.con.close()
 
   def _handle_ConnectionClosed (self, event):
+    """
+    Encerra o tratamento da conexão
+    :param event: Evento de chama deste método.
+    :return: sem retorno.
+    """
     logging.getLogger().removeHandler(self)
 
-  def emit (self, record):
-    o = {'message' : self.format(record)}
+  def emit(self, record):
+    """
+    Faz o que for preciso para realmente registrar o registro de log especificado.
+
+    :param record: Log gravado.
+    :return: Sem retorno.
+    """
+    o = {'message': self.format(record)}
     if self._opaque is not None:
       o.update(self._opaque)
-    #o['message'] = record.getMessage()
+    # o['message'] = record.getMessage()
     if self._json:
       for attr in _attributes:
         o[attr] = getattr(record, attr)
@@ -112,6 +147,14 @@ class LogMessenger (logging.Handler):
 
 class LogMessengerListener (object):
   """
+  Toma cuidado de spawning individual LogMessengers
+
+  Olá mensagem é como:
+  {"Hello": "log"}
+  Você também pode incluir qualquer um dos parâmetros de configuração para LogMessenger
+  (Como "nível").
+  """
+  """
   Takes care of spawning individual LogMessengers
 
   Hello message is like:
@@ -119,10 +162,16 @@ class LogMessengerListener (object):
   You can also include any of the config parameters for LogMessenger
   (like "level").
   """
-  def __init__ (self):
+  def __init__(self):
     core.messenger.addListener(MessageReceived, self._handle_global_MessageReceived)
 
   def _handle_global_MessageReceived (self, event, msg):
+    """
+    Comfirma eventos de log.
+    :param event: Evento que causa a chamada desse método.
+    :param msg: Dicionãrio de informações da mensagem.
+    :return: Retorna True se o evento for confirmado, ou None se houver algúm erro.
+    """
     try:
       json = False
       if msg['hello'] == 'log':
@@ -137,7 +186,11 @@ class LogMessengerListener (object):
       pass
 
 
-def launch ():
+def launch():
+  """
+  Lança o componente LogMessengerListener no core.
+  :return: Sem retorno.
+  """
   def realStart (event=None):
     if not core.hasComponent("messenger"):
       if event is None:
