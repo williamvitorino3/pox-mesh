987e508efd35c135e8fc02bf310c21ad23a903d9
william <williamvitorino3@gmail.com>
2017-02-13 09:02:45 -0300

    Documantação do arquivo lib.mock_socket.py  adicionada.

--- pox/lib/mock_socket.py
+++ pox/lib/mock_socket.py
@@ -1,92 +1,109 @@
-# Copyright 2012 Andreas Wundsam
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """
-This module provides a MockSocket that can be used to fake TCP
-connections inside of the simulator
+Este módulo fornece um MockSocket que pode ser usado para conexões TCP falsas dentro do simulador.
+
+This module provides a MockSocket that can be used to fake TCP connections inside of the simulator
 """
 
-class MockSocket (object):
+class MockSocket:
   """
-  A mock socket that works on a sending and a receiving message channel.
-  Use MockSocket.pair() to get a pair of connected MockSockets
+      Um Mock simulado que funciona em um canal de envio e um de mensagem de recebimento.
+      Use MockSocket.pair () para obter um par de MockSockets conectados
 
-  TODO: model failure modes
+      A mock socket that works on a sending and a receiving message channel. Use
+      MockSocket.pair() to get a pair of connected MockSockets
+
+      TODO: model failure modes
   """
   def __init__(self, receiving, sending):
     self.receiving = receiving
     self.sending = sending
 
-  def send (self, data):
+  def send(self, data):
     """
-    Send data out on this socket.
-
-    Data will be available for reading at the receiving socket pair.
-    Note that this currently always succeeds and never blocks (unlimited
-    receive buffer size)
+    Envia dados de saída..
+    :param data: Dados a serem enviados.
+    :return: Objeto de envio.
+    """
+    """ Send data out on this socket. Data will be available for reading at the receiving
+        socket pair. Note that this currently always succeeds and never blocks (unlimited
+        receive buffer size)
     """
     return self.sending.send(data)
 
-  def recv (self, max_size=None):
+  def recv(self, max_size=None):
     """
-    receive data on this sockect.
-
-    If no data is available to be received, return "".
-    Note that this is non-standard socket behavior and should be
-    changed to mimic either blocking on non-blocking socket semantics
+    Recebe informações do socket.
+    :param max_size: Tamanho máximo do buffer.
+    :return: Dados recebidos.
+    """
+    """ receive data on this sockect. If no data is available to be received, return "".
+        NOTE that this is non-standard socket behavior and should be changed to mimic either
+        blocking on non-blocking socket semantics
     """
     return self.receiving.recv(max_size)
 
-  def set_on_ready_to_recv (self, on_ready):
+  def set_on_ready_to_recv(self, on_ready):
     """
-    set a handler function on_ready(socket, size) to be called when
-    data is available for reading at this socket
+    Manipula on_ready (socket, size) para ser chamado quando os dados estão disponíveis
+    para leitura neste soquete
+    :param on_ready: Método à ser chamado.
+    :return: Sem retorno.
     """
+    """ set a handler function on_ready(socket, size) to be called when data is available
+    for reading at this socket """
     self.receiving.on_data = lambda channel, size: on_ready(self, size)
 
-  def ready_to_recv (self):
+  def ready_to_recv(self):
+    """
+    Verifica se o socket está pronto para receber dados.
+    :return: Booleano.
+    """
     return not self.receiving.is_empty()
 
-  def ready_to_send (self):
+  def ready_to_send(self):
+    """
+    Verifica se o socket está pronto para enviar dados.
+    :return: Booleano.
+    """
     return self.sending.is_full()
 
-  def shutdown (self, sig=None):
+  def shutdown(self, sig=None):
     """
-    shutdown a socket.
-    Currently a no-op on this MockSocket object.
+    Desliga o socket.
+    :param sig: Argumento não utilizado.
+    :return: Sem retorno.
     """
-    pass
-    #TODO: implement more realistic closing semantics
-
-  def close (self):
-    """
-    close a socket. Currently a no-op on this MockSocket object.
+    """ shutdown a socket. Currently a no-op on this MockSocket object.
+        TODO: implement more realistic closing semantics
     """
     pass
-    #TODO: implement more realistic closing semantics
 
-  def fileno (self):
+  def close(self):
     """
-    return the pseudo-fileno of this Mock Socket.
-    Currently always returns -1.
+    Fecha o socket.
+    :return: Sem retorno.
+    """
+    """ close a socket. Currently a no-op on this MockSocket object.
+        TODO: implement more realistic closing semantics
+    """
+    pass
+
+  def fileno(self):
+    """
+    Pega o pseudo-fileno deste Mock Socket
+    :return: Retornar o pseudo-fileno deste Mock Socket.
+    """
+    """ return the pseudo-fileno of this Mock Socket. Currently always returns -1.
+        TODO: assign unique pseudo-filenos to mock sockets, so apps don't get confused.
     """
     return -1
-    #TODO: assign unique pseudo-filenos to mock sockets,
-    #      so apps don't get confused.
 
   @classmethod
-  def pair (cls):
+  def pair(cls):
+    """
+    Retorna um par de soquetes conectados.
+    :return: Par de soquetes conectados.
+    """
     """ Return a pair of connected sockets """
     a_to_b = MessageChannel()
     b_to_a = MessageChannel()
@@ -94,24 +111,34 @@ class MockSocket (object):
     b = cls(b_to_a, a_to_b)
     return (a,b)
 
-class MessageChannel (object):
+class MessageChannel(object):
   """
-  A undirectional reliable in order byte stream message channel
-  (think TCP half-connection)
+  Um unidirecional confiável em ordem byte transmitir canal de mensagens.
   """
-  def __init__ (self):
+  """ A undirectional reliable in order byte stream message channel (think TCP half-connection)
+  """
+  def __init__(self):
     # Single element queue
     self.buffer = ""
     self.on_data = None
     self.on_data_running = False
     self.pending_on_datas = 0
 
-  def send (self, msg):
+  def send(self, msg):
+    """
+    Envia mensagens.
+    :param msg: Mensagem à ser enviada.
+    :return: Tamanho da mensagem.
+    """
     self.buffer += msg
     self._trigger_on_data()
     return len(msg)
 
-  def _trigger_on_data (self):
+  def _trigger_on_data(self):
+    """
+    Redimensiona o tamanho do buffer.
+    :return: Sem retorno.
+    """
     self.pending_on_datas += 1
     if self.on_data_running:
       # avoid recursive calls to on_data
@@ -126,11 +153,13 @@ class MessageChannel (object):
       else:
         break
 
-  def recv (self, max_size=None):
+  def recv(self, max_size=None):
     """
-    retrieve and return the data stored in this channel's buffer.
-    If buffer is empty, return ""
+    Recupera e retornar os dados armazenados no buffer deste canal.
+    :param max_size: tamanho máximo do buffer.
+    :return: Mensagem recebida.
     """
+    """ retrieve and return the data stored in this channel's buffer. If buffer is empty, return "" """
     if max_size and max_size < len(self.buffer):
       msg = self.buffer[0:max_size]
       self.buffer = self.buffer[max_size:]
@@ -139,12 +168,20 @@ class MessageChannel (object):
       self.buffer = ""
     return msg
 
-  def is_empty (self):
+  def is_empty(self):
+    """
+    Verifica se o buffer está vazio.
+    :return: Booleano.
+    """
     return len(self.buffer) == 0
 
-  def is_full (self):
+  def is_full(self):
+    """
+    Verifica se o buffer está cheio.
+    :return: Falso (por quê o buffer sempre pode almentar de tamanho.).
+    """
     #  buffer length not constrained currently
     return False
 
-  def __len__ (self):
+  def __len__(self):
     return len(self.buffer)
